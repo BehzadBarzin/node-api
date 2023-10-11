b5dda9ad3530c42049c255aa26bc210e249027ea
Behzad Barzin <Behzad_Barzin@live.com>
2023-10-12 01:04:05 +0330

    .

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,14 +2,16 @@ import { Request, Response } from 'express';
 import log from '../utils/logger';
 import { createUser } from '../services/user.service';
 import { CreateUserInput } from '../schemas/user.schema';
-import mongoose from 'mongoose';
+import { Omit, omit } from 'lodash';
 
 export async function createUserHandler(req: Request<{}, {}, CreateUserInput['body']>, res: Response) {
     try {
         const user = await createUser(req.body);
-        return res.send(user);
+
+        // Send back user without password
+        return res.send(omit(user.toJSON(), 'password'));
     } catch (error: any) {
-        log.error(error);
+        // log.error(error);
         // 409 Means conflict: we're assuming that if this function throws an error, it's because the uniqueness of email of user was violated.
         return res.status(409).send(error.message);
     }
